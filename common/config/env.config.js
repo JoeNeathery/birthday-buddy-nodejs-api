c63653f9251a78a2066b5ea3b025b4a5245f53bd
Joe Neeathery <jneathery1201@gmail.com>
2020-06-10 22:19:11 -0400

    update config

--- common/config/env.config.js
+++ common/config/env.config.js
@@ -21,8 +21,8 @@ var config = {
     },
     production: {
         server: {
-            appEndpoint: "http://52.87.42.240/3600",
-            apiEndpoint: "http://52.87.42.240/3600",
+            appEndpoint: "http://52.87.42.240:3600",
+            apiEndpoint: "http://52.87.42.240:3600",
             port: 3600,
             jwt_secret: "myS33!!creeeT",
             jwt_expiration_in_seconds: 36000
